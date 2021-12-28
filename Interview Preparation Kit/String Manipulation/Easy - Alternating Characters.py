c7716a94529dc7e1bec604d493f5085f891e034f
joko triyanto <jokotriyanto123@outlook.com>
2021-12-28 23:12:07 +0700

    Aternating Characters

---
+++ Interview Preparation Kit/String Manipulation/Easy - Alternating Characters.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'alternatingCharacters' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def alternatingCharacters(s):
+    # Write your code here
+    count = 0
+    last_char = s[0]
+    for c in s[1:]:
+        if c == last_char:
+            count += 1
+        last_char = c
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        s = input()
+
+        result = alternatingCharacters(s)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
