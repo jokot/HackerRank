76756ac8d20ea0845dc9dfa90d9ab95ffcbade4e
joko triyanto <jokotriyanto123@outlook.com>
2022-01-19 09:04:19 +0700

    Day 1 - 30 Days of Code

---
+++ 30 Days of Code/Day 1 - Data Type.py
@@ -0,0 +1,18 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+iS = 0
+dS = 0.0
+sS = ''
+# Read and save an integer, double, and String to your variables.
+iS = int(input())
+dS = float(input())
+sS = input()
+# Print the sum of both integer variables on a new line.
+print(i+iS)
+# Print the sum of the double variables on a new line.
+print(d+dS)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+sS)
